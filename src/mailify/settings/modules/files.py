46f37e27401d1703d390c9e0964d2ae43058da4b
Michael van de Waeter <mvdwaeter@gmail.com>
2016-03-06 16:12:01 +0100

    Improved heroku setup

--- src/mailify/settings/modules/files.py
+++ src/mailify/settings/modules/files.py
@@ -7,3 +7,5 @@ STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media')
 MEDIA_URL = '/media/'
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
