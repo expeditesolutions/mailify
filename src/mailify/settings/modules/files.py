8899fd5d53a1b3cb6cc5956a987dace6a51d67ec
Michael van de Waeter <mvdwaeter@gmail.com>
2016-03-06 16:19:49 +0100

    Added favicon

--- src/mailify/settings/modules/files.py
+++ src/mailify/settings/modules/files.py
@@ -9,3 +9,5 @@ MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media')
 MEDIA_URL = '/media/'
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+FAVICON_PATH = STATIC_URL + 'images/favicon.png'
--- src/mailify/urls.py
+++ src/mailify/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import include, url
+from django.views.generic import RedirectView
 from django.contrib import admin
 
 
@@ -6,4 +8,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api/', include('rest_api.urls', namespace='rest_api')),
+    url(r'^favicon\.ico$', RedirectView.as_view(url=settings.FAVICON_PATH, permanent=True), name='favicon'),
 ]
