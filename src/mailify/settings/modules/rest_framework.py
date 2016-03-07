2a4ecfba9f7a1275fdbb5af8552cb4c8579dabb3
Michael van de Waeter <mvdwaeter@gmail.com>
2016-03-07 18:15:08 +0100

    Removed browsable API

--- src/mailify/settings/modules/rest_framework.py
+++ src/mailify/settings/modules/rest_framework.py
@@ -5,6 +5,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [],
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
+        # 'rest_framework.renderers.BrowsableAPIRenderer',
     ),
 }
