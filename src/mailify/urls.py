c713ffa633cf20ccd94e845062b8ebaf952f6221
Michael van de Waeter <mvdwaeter@gmail.com>
2016-03-08 20:55:38 +0100

    Added robots.txt to not index this website

--- src/mailify/urls.py
+++ src/mailify/urls.py
@@ -1,10 +1,12 @@
 from django.conf import settings
 from django.conf.urls import include, url
+from django.http import HttpResponse
 from django.views.generic import RedirectView
 from django.contrib import admin
 
 
 urlpatterns = [
+    url(r'^robots.txt$', lambda r: HttpResponse('User-agent: *\nDisallow: /', content_type='text/plain')),
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api/', include('rest_api.urls', namespace='rest_api')),
