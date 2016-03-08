4beb078098d93048ad19e261e728ff03a78743b0
Michael van de Waeter <mvdwaeter@gmail.com>
2016-03-08 14:20:25 +0100

    Changed email from setting

--- src/mailify/settings/modules/email.py
+++ src/mailify/settings/modules/email.py
@@ -4,7 +4,7 @@ ADMINS = (
     ('Michael van de Waeter', 'info@ideallical.com'),
 )
 
-DEFAULT_FROM_EMAIL = 'Goldsmiths <send@mail.goldsmiths.co.uk>'
+DEFAULT_FROM_EMAIL = 'Goldsmiths <noreply@mail.goldsmiths.co.uk>'
 
 EMAIL_BACKEND = 'sgbackend.SendGridBackend'
 SENDGRID_API_KEY = get_env_variable('SENDGRID_API_KEY')
